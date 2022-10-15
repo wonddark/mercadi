6a9186b1f39756262230bab605dbacab27d97b31
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-15 11:27:21 -0400

    Finished the user bids page

--- src/components/UserBids.tsx
+++ src/components/UserBids.tsx
@@ -1,5 +1,8 @@
 import useGoBack from "../hooks/go-back.hooks";
 import { Button, Container } from "reactstrap";
+import HeaderBid from "./HeaderBid";
+import { dummyBids } from "../data/dummy-bids";
+import RowBid from "./RowBid";
 
 function UserBids() {
   document.title = "Pujas";
@@ -9,6 +12,18 @@ function UserBids() {
       <Button onClick={goBackToFeed}>
         <i className="bi bi-arrow-left-circle-fill" />
       </Button>
+      <HeaderBid />
+      <div
+        className="pe-1"
+        style={{
+          height: "calc(65vh)",
+          overflow: "auto",
+        }}
+      >
+        {dummyBids.map((item) => (
+          <RowBid item={item} key={item.id} />
+        ))}
+      </div>
     </Container>
   );
 }

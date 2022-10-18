158b138bbd7ea3c1b07ed4730bf16834128245d9
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-18 10:09:39 -0400

    Used the isLogged selector to show/hide parts of navbar

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -14,6 +14,8 @@ import AddOffer from "./AddOffer";
 import useSessionMenuActions from "../hooks/session-menu.hooks";
 import Login from "./Login";
 import Register from "./Register";
+import { useSelector } from "react-redux";
+import { selectIsLogged } from "../state/slices/session";
 
 function UserNav() {
   const { isOpen: addArticle, toggleIsOpen: toggleAddArticle } =
@@ -25,63 +27,72 @@ function UserNav() {
   const { goToOffers, goToBids, goToFeed } = useSessionMenuActions(
     toggleSessionMenuOpen
   );
+  const isLogged = useSelector(selectIsLogged);
   return (
     <header>
       <Navbar dark color="dark" container expand>
         <NavbarBrand>Casa de Subastas</NavbarBrand>
         <Nav navbar className="ms-auto">
-          <NavItem className="me-1">
-            <Button
-              color="primary"
-              aria-label="Agregar oferta"
-              title="Agregar oferta"
-              onClick={toggleAddArticle}
-            >
-              <i className="bi bi-plus-circle-fill" />
-            </Button>
-          </NavItem>
-          <NavItem className="me-1">
-            <Button
-              aria-label="Notifications"
-              title="Notificaciones"
-              color="transparent"
-              className="text-white"
-            >
-              <i className="bi bi-bell" />
-            </Button>
-          </NavItem>
-          <Dropdown
-            isOpen={sessionMenuOpen}
-            toggle={toggleSessionMenuOpen}
-            nav
-            inNavbar
-            direction="end"
-          >
-            <DropdownToggle
-              nav
-              title="Perfil"
-              color="transparent"
-              className="text-white"
-            >
-              <i className="bi bi-person-circle" />
-            </DropdownToggle>
-            <DropdownMenu dark>
-              <DropdownItem onClick={goToFeed}>Feed</DropdownItem>
-              <DropdownItem onClick={goToOffers}>Tus ofertas</DropdownItem>
-              <DropdownItem onClick={goToBids}>Tus pujas</DropdownItem>
-              <DropdownItem>Cerrar sesión</DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-          <NavItem className="me-1">
-            <Button color="light" outline onClick={toggleLogin}>
-              Acceder
-            </Button>
-          </NavItem>
-          <NavItem>
-            <Button color="primary" onClick={toggleRegister}>
-              Crear cuenta
-            </Button>
-          </NavItem>
+          {isLogged && (
+            <>
+              <NavItem className="me-1">
+                <Button
+                  color="primary"
+                  aria-label="Agregar oferta"
+                  title="Agregar oferta"
+                  onClick={toggleAddArticle}
+                >
+                  <i className="bi bi-plus-circle-fill" />
+                </Button>
+              </NavItem>
+              <NavItem className="me-1">
+                <Button
+                  aria-label="Notifications"
+                  title="Notificaciones"
+                  color="transparent"
+                  className="text-white"
+                >
+                  <i className="bi bi-bell" />
+                </Button>
+              </NavItem>
+              <Dropdown
+                isOpen={sessionMenuOpen}
+                toggle={toggleSessionMenuOpen}
+                nav
+                inNavbar
+                direction="end"
+              >
+                <DropdownToggle
+                  nav
+                  title="Perfil"
+                  color="transparent"
+                  className="text-white"
+                >
+                  <i className="bi bi-person-circle" />
+                </DropdownToggle>
+                <DropdownMenu dark>
+                  <DropdownItem onClick={goToFeed}>Feed</DropdownItem>
+                  <DropdownItem onClick={goToOffers}>Tus ofertas</DropdownItem>
+                  <DropdownItem onClick={goToBids}>Tus pujas</DropdownItem>
+                  <DropdownItem>Cerrar sesión</DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </>
+          )}
+          {!isLogged && (
+            <>
+              <NavItem className="me-1">
+                <Button color="light" outline onClick={toggleLogin}>
+                  Acceder
+                </Button>
+              </NavItem>
+              <NavItem>
+                <Button color="primary" onClick={toggleRegister}>
+                  Crear cuenta
+                </Button>
+              </NavItem>
+            </>
+          )}
         </Nav>
       </Navbar>
       {addArticle && <AddOffer isOpen={addArticle} toggle={toggleAddArticle} />}

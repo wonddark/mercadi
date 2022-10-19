6953a231fb01dd6b2031f7a76e87f4b0a38a56c0
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-18 22:10:43 -0400

    Removed the reactstrap dependencies from OfferQuickView component

--- src/components/OfferQuickView.tsx
+++ src/components/OfferQuickView.tsx
@@ -1,4 +1,3 @@
-import { Button, Card, Col, Container, Row } from "reactstrap";
 import useToggleOpen from "../hooks/toggle-open.hooks";
 import CloseOffer from "./CloseOffer";
 import { formatMoney } from "../helpers/formatters.helper";
@@ -12,36 +11,35 @@ interface Props {
 function OfferQuickView({ name, initialOffer, currentOffer }: Props) {
   const { isOpen, toggleIsOpen } = useToggleOpen();
   return (
-    <Card body className="my-2">
-      <Container>
-        <Row>
-          <Col xs={12}>
+    <div className="card card-body my-2">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col col-12">
             <span className="h6">{name}</span>
-          </Col>
-          <Row className="justify-content-between">
-            <Col xs={10}>
+          </div>
+          <div className="row justify-content-between">
+            <div className="col col-10">
               <span className="small text-muted d-block">
                 Puja inicial: <strong>{formatMoney(initialOffer)}</strong>
               </span>
               <span className="small text-muted d-block">
                 Puja actual: <strong>{formatMoney(currentOffer)}</strong>
               </span>
-            </Col>
-            <Col xs={1} className="mt-2">
-              <Button
-                color="primary"
+            </div>
+            <div className="col col-1 mt-2">
+              <button
+                className="btn btn-primary btn-sm"
                 onClick={toggleIsOpen}
-                size="sm"
                 title="Cerrar oferta"
               >
                 <i className="bi bi-x-circle-fill" />
-              </Button>
-            </Col>
-          </Row>
-        </Row>
-      </Container>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       {isOpen && <CloseOffer isOpen={isOpen} toggle={toggleIsOpen} />}
-    </Card>
+    </div>
   );
 }
 

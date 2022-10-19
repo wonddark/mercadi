f0d77d65b227d0630128009292aaf83296f0d170
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-18 21:59:41 -0400

    Redesigned the form for offers and removed the reactstrap dependencies

--- src/components/AddOffer.tsx
+++ src/components/AddOffer.tsx
@@ -1,6 +1,5 @@
 import CommonDlg from "./CommonDlg";
 import React from "react";
-import { Form, FormGroup, Input } from "reactstrap";
 
 interface Props {
   isOpen: boolean;
@@ -8,43 +7,56 @@ interface Props {
 }
 function AddOffer({ isOpen, toggle }: Props) {
   const form = (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Título para la oferta" name="offer-name" />
-      </FormGroup>
-      <FormGroup>
-        <Input
+    <form>
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Título para la oferta"
+          name="offer-name"
+          id="offer-name"
+        />
+        <label htmlFor="offer-name">Título</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
           type="number"
           min={50}
           max={1000000}
+          className="form-control"
           placeholder="Puja inicial"
           name="offer-initial-bid"
+          id="offer-initial-bid"
         />
-      </FormGroup>
-      <FormGroup>
-        <Input
-          type="textarea"
+        <label htmlFor="offer-initial-bid">Puja inicial</label>
+      </div>
+      <div className="form-floating mb-3">
+        <textarea
+          className="form-control"
           placeholder="Descripción de la oferta"
           name="offer-description"
+          id="offer-description"
+          style={{ height: "180px" }}
         />
-      </FormGroup>
-      <FormGroup>
-        <Input
+        <label htmlFor="offer-description">Descripción</label>
+      </div>
+      <div className="mb-3">
+        <input
           type="file"
+          className="form-control"
           placeholder="Agrega imágenes"
           name="offer-files"
           accept="image/png, image/jpeg"
           multiple
         />
-      </FormGroup>
-    </Form>
+      </div>
+    </form>
   );
   return (
     <CommonDlg
       isOpen={isOpen}
       toggle={toggle}
       title="Agregar oferta"
-      titleColor="success"
       icon={<i className="bi bi-plus-circle-fill me-2" />}
       size="md"
       content={form}

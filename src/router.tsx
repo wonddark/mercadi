2a5e4f1a3b35b5dab38c7f84700467758e23e395
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-25 12:46:03 -0400

    Added route "/registration/confirm"

--- src/router.tsx
+++ src/router.tsx
@@ -8,11 +8,13 @@ import Home from "./components/Home";
 import UserHome from "./components/UserHome";
 import UserOffers from "./components/UserOffers";
 import UserBids from "./components/UserBids";
+import ConfirmRegistration from "./components/ConfirmRegistration";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<Home />} />
+      <Route path="registration/confirm" element={<ConfirmRegistration />} />
       <Route path="user" element={<UserHome />}>
         <Route path="feed" element={<HomeFeed />} />
         <Route path="offers" element={<UserOffers />} />

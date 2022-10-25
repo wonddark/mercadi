84a78359f3fb90043b62304bdb0b092a3475191d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-25 12:56:06 -0400

    At home component handled the route cases for child routes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 function Home() {
+  const { pathname } = useLocation();
   return (
-    <h1>
-      Welcome to our auction house{" "}
-      <NavLink to="/user/feed">Go to your fed</NavLink>
-    </h1>
+    <div className="container-fluid p-0">
+      {pathname === "/" ? (
+        <h1>
+          Bienvenido a Subastia
+          <NavLink to="/user/feed">lo último que tenemos para ti</NavLink>
+        </h1>
+      ) : (
+        <Outlet />
+      )}
+    </div>
   );
 }
 

451939b431285745aebd2257e6c1c711cbf24f25
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-29 16:25:32 -0400

    Finished the page for registration pending of confirmation

--- src/components/ConfirmRegistration.tsx
+++ src/components/ConfirmRegistration.tsx
@@ -1,13 +1,32 @@
-import { useLocation } from "react-router-dom";
+import { ReactComponent as EmailSent } from "../assets/undraw_mail_sent.svg";
 
 function ConfirmRegistration() {
-  const { state } = useLocation();
   return (
     <div className="container-fluid">
-      Hola {state?.["name"]}, nos enorgullece que quieras formar parte de
-      nuestro club. Te hemos enviado un email de confirmación a la dirección{" "}
-      {state?.["email"]}. Por favor, revisa tu bandeja de entrada y activa tu
-      cuenta, te estamos esperando!
+      <div className="px-4 py-5 my-5 text-center">
+        <EmailSent
+          style={{ width: "80px", height: "auto" }}
+          className="mx-auto mb-4"
+        />
+        <h1 className="display-5 fw-bold">Pendiente</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            Hemos recibido tu solicitud y la hemos respondido con un mensaje que
+            contiene los pasos necesarios para confirmar tu registro en
+            Subastia, por favor, ve a tu bandeja de entrada, confirma el
+            registro y nos vemos aquí de nuevo
+          </p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-lg px-4"
+              disabled
+            >
+              Volver a enviar
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

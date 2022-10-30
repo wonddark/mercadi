1c9c9e8c8343fe2d62d921914a599678532086b6
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-10-30 11:16:21 -0400

    Added the route "/registration/activate/:registrationId"

--- src/router.tsx
+++ src/router.tsx
@@ -9,12 +9,20 @@ import UserOffers from "./components/UserOffers";
 import UserBids from "./components/UserBids";
 import ConfirmRegistration from "./components/ConfirmRegistration";
 import StaticLayoutContainer from "./components/StaticLayoutContainer";
+import ActivateRegistration from "./components/ActivateRegistration";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<StaticLayoutContainer />} />
-      <Route path="registration/confirm" element={<ConfirmRegistration />} />
+      <Route path="registration">
+        <Route path="confirm" element={<ConfirmRegistration />} />
+        <Route
+          path="activate/:registrationId"
+          element={<ActivateRegistration />}
+        />
+      </Route>
+
       <Route path="user" element={<UserHome />}>
         <Route path="feed" element={<HomeFeed />} />
         <Route path="offers" element={<UserOffers />} />

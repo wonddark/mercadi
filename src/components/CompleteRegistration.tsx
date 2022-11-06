e576ceb9fb40816fe4a0975e54da2f6ddf24538f
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-06 00:00:13 -0500

    Component for completing registration

---
+++ src/components/CompleteRegistration.tsx
@@ -0,0 +1,66 @@
+import { Controller } from "react-hook-form";
+import useCompleteRegistration from "../hooks/complete-registration.hook";
+
+function CompleteRegistration() {
+  const { control, submitForm } = useCompleteRegistration();
+  return (
+    <div className="container col-xl-10 col-xxl-8 px-4">
+      <div className="row align-items-center py-3">
+        <div className="col-md-10 mx-auto col-lg-5">
+          <p className="p-2 lead">
+            Aún no sabemos nada de ti además de tu email, quisiéramos conocer
+            algunos datos más
+          </p>
+          <form
+            onSubmit={submitForm}
+            className="p-4 p-md-5 border rounded-3 bg-light bg-gradient"
+          >
+            <Controller
+              name="name"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className={`form-control ${error ? "is-invalid" : ""}`}
+                    id="floatingInput"
+                    placeholder="nombre@dominio.com"
+                    {...field}
+                  />
+                  <label htmlFor="floatingInput">Nombre (requerido)</label>
+                  {error && (
+                    <div className="invalid-feedback">{error.message}</div>
+                  )}
+                </div>
+              )}
+            />
+            <Controller
+              name="lastname"
+              control={control}
+              render={({ field, fieldState: { error } }) => (
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className={`form-control ${error ? "is-invalid" : ""}`}
+                    id="floatingInput"
+                    placeholder="nombre@dominio.com"
+                    {...field}
+                  />
+                  <label htmlFor="floatingInput">Apellidos (requerido)</label>
+                  {error && (
+                    <div className="invalid-feedback">{error.message}</div>
+                  )}
+                </div>
+              )}
+            />
+            <button className="w-100 btn btn-lg btn-primary" type="submit">
+              Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompleteRegistration;
---
+++ src/hooks/complete-registration.hook.ts
@@ -0,0 +1,49 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useUpdateUserDataMutation } from "../state/services/api";
+import { useAppSelector } from "./state.hooks";
+import { selectId, selectName } from "../state/slices/session";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function useCompleteRegistration() {
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      name: "",
+      lastname: "",
+    },
+    mode: "onChange",
+    resolver: yupResolver(
+      yup.object({
+        name: yup.string().required(),
+        lastname: yup.string().required(),
+      })
+    ),
+  });
+  const state = useAppSelector((state) => state);
+  const id = selectId(state);
+  const name = selectName(state);
+  const [updateUserData, { isLoading, isError }] = useUpdateUserDataMutation();
+  const navigate = useNavigate();
+
+  const completeRegistration = (data: { name: string; lastname: string }) => {
+    updateUserData({ id, name: data.name, lastname: data.lastname });
+  };
+
+  const submitForm = handleSubmit(completeRegistration);
+
+  const goToUserFeed = () => {
+    name && navigate("/user/feed");
+  };
+
+  useEffect(
+    goToUserFeed,
+    // eslint-disable-next-line
+    [name]
+  );
+
+  return { control, submitForm, isLoading, isError };
+}
+
+export default useCompleteRegistration;

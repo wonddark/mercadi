14e9b46e9799b073137fad35ce5347198df20b99
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-22 15:02:12 -0500

    Small adjustments to navbar for not-logged users

--- src/components/NotLoggedNavItems.tsx
+++ src/components/NotLoggedNavItems.tsx
@@ -4,13 +4,12 @@ function NotLoggedNavItems() {
   return (
     <>
       <li className="nav-item me-1">
-        <Link to="/acceder" className="btn btn-light">
-          <i className="bi bi-box-arrow-in-right me-md-2" />
+        <Link to="/acceder" className="nav-link link-light">
           <span className="d-none d-md-inline">Acceder</span>
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/registro" className="btn btn-primary">
+        <Link to="/registro" className="btn btn-success">
           <i className="bi bi-person-plus-fill me-md-2" />
           <span className="d-none d-md-inline">Crear cuenta</span>
         </Link>

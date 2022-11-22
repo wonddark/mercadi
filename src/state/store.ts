30d5dfbe1978ae34b83934bf5d4624e7f946d56a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-22 18:11:44 -0500

    Save/read global state value to/from local storage

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,11 +2,29 @@ import { configureStore } from "@reduxjs/toolkit";
 import sessionReducer, { SESSION_STORE_KEY } from "./slices/session";
 import { API_STORE_KEY, reducer as apiReducer } from "./services/api";
 
+const STORAGE_KEY = "subastia";
+const persistedState = (() => {
+  try {
+    const rawState = localStorage.getItem(STORAGE_KEY);
+    if (rawState) return JSON.parse(rawState);
+    return undefined;
+  } catch (e) {
+    return undefined;
+  }
+})();
+
 export const store = configureStore({
   reducer: {
     [SESSION_STORE_KEY]: sessionReducer,
     [API_STORE_KEY]: apiReducer,
   },
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  const stateToStore = JSON.parse(JSON.stringify(store.getState()));
+  process.env.NODE_ENV === "production" && delete stateToStore[API_STORE_KEY];
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(stateToStore));
 });
 
 export type RootState = ReturnType<typeof store.getState>;

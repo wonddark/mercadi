bb931be61995b68ca2b2ba318003aa5638d75fd3
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-22 18:13:46 -0500

    This component will be only a container for descending routes

--- src/components/UserHome.tsx
+++ src/components/UserHome.tsx
@@ -1,15 +1,7 @@
 import { Outlet } from "react-router-dom";
-import UserNav from "./UserNav";
 
 function UserHome() {
-  return (
-    <div className="container-fluid p-0">
-      <UserNav />
-      <div className="mt-3">
-        <Outlet />
-      </div>
-    </div>
-  );
+  return <Outlet />;
 }
 
 export default UserHome;

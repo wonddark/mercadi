588f8a8c91f501fa2302eed5c94e122a95d883c4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-26 23:34:33 -0500

    Placeholder for quick offer

---
+++ src/components/article/OfferQuickViewPlaceholder.tsx
@@ -0,0 +1,14 @@
+function OfferQuickViewPlaceholder() {
+  return (
+    <div className="card card-body my-2">
+      <div className="container-fluid p-0">
+        <div className="row justify-content-between placeholder-glow">
+          <div className="col-10 placeholder" style={{ minHeight: 10 }} />
+          <div className="col-8 placeholder mt-1" style={{ minHeight: 10 }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OfferQuickViewPlaceholder;

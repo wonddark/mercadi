67338e7eb4f59211a7464076ff91f9c3f563b240
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-26 23:58:06 -0500

    Placeholder component for offer quick view

---
+++ src/components/bid/BidsQuickViewPlaceholder.tsx
@@ -0,0 +1,17 @@
+function BidsQuickViewPlaceholder() {
+  return (
+    <div className="card card-body my-2">
+      <div className="container-fluid p-0">
+        <div className="row justify-content-between">
+          <div className="col col-9">
+            <span className="h6"></span>
+            <span className="small text-muted d-block"></span>
+            <span className="small text-muted d-block"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BidsQuickViewPlaceholder;

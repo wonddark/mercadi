9e440660a07ddaf0106d95f2534bb6d3af511d3c
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-11-29 22:00:48 -0500

    Updated the component UserHome to hold the components for descendant routes

--- src/components/user/UserHome.tsx
+++ src/components/user/UserHome.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from "react-router-dom";
+import useGoBack from "../../hooks/go-back.hooks";
 
 function UserHome() {
-  return <Outlet />;
+  const { goBackTo: goBackToFeed } = useGoBack("/muro");
+  return (
+    <div className="container-fluid mt-3">
+      <button className="btn btn-secondary" onClick={goBackToFeed}>
+        <i className="bi bi-arrow-left-circle-fill" />
+      </button>
+      <Outlet />
+    </div>
+  );
 }
 
 export default UserHome;

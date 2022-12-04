8601e347cb51e9085ae740fd54c5b117d370fdcb
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-03 23:11:23 -0500

    Updated the design of offers in feed
    -- Changed places between higher offer tag and published date

--- src/components/article/FeedOffer.tsx
+++ src/components/article/FeedOffer.tsx
@@ -34,9 +34,8 @@ function FeedOffer({ item }: Props) {
             <span className="small">
               {`${item.user.name} ${item.user.lastname}`}
             </span>
-            <span className="ms-2 py-1 px-2 small fw-bold bg-light-gray text-primary">
-              <i className="bi bi-flag-fill me-1" />
-              {formatMoney(item.highestBid.quantity)}
+            <span className="ms-2 small text-muted">
+              {dayjs(item.publishedAt).format("DD/MM/YYYY HH:mm")}
             </span>
             <div className="container">
               <div className="row gap-1 my-3">
@@ -54,11 +53,12 @@ function FeedOffer({ item }: Props) {
               </div>
             </div>
             <p className="m-0">{item.description}</p>
-            <p className="m-0 text-muted">
-              {dayjs(item.publishedAt).format("DD/MM/YYYY HH:mm")}
-            </p>
+            <span className="d-block mt-3 small fw-bold text-primary">
+              <i className="bi bi-flag-fill me-1" />
+              {formatMoney(item.highestBid.quantity)}
+            </span>
             {item.user.id !== userId && (
-              <div className="mt-3">
+              <div className="mt-1">
                 <PostBidBtn
                   offerId={item["@id"]}
                   highestBid={item.highestBid.quantity}

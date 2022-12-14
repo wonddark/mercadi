aef8efe5d1569984772ca9200a688e6b2177d0b2
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-14 07:54:25 -0500

    Fixed the test condition used to show quick access sidebar

--- src/components/HomeFeed.tsx
+++ src/components/HomeFeed.tsx
@@ -1,13 +1,14 @@
 import FeedQuickAccess from "./FeedQuickAccess";
 import FeedsColumn from "./FeedsColumn";
 import { useAppSelector } from "../hooks/state.hooks";
-import { selectIsLogged } from "../state/slices/session";
+import { selectId, selectIsLogged } from "../state/slices/session";
 import { pageTitle } from "../helpers/page-title.helper";
 import MustBeAuthenticated from "./MustBeAuthenticated";
 
 function HomeFeed() {
   const state = useAppSelector((state) => state);
   const isLogged = selectIsLogged(state);
+  const userId = selectId(state);
   pageTitle("Home");
   return (
     <div className="container-xxl mt-3">
@@ -16,8 +17,7 @@ function HomeFeed() {
           <FeedsColumn />
         </div>
         <div className="col d-none d-lg-block col-lg-4">
-          {isLogged && <FeedQuickAccess />}
-          {!isLogged && <MustBeAuthenticated />}
+          {isLogged && userId ? <FeedQuickAccess /> : <MustBeAuthenticated />}
         </div>
       </div>
     </div>

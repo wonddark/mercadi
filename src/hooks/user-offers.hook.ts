e12ffe2d87e7c04882acab175453510841e76a9c
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-12-14 13:54:38 -0500

    Updated the useUserOffers hook
    -- Using an optional parameter to filter open offers

--- src/hooks/user-offers.hook.ts
+++ src/hooks/user-offers.hook.ts
@@ -2,12 +2,19 @@ import { useAppSelector } from "./state.hooks";
 import { selectId } from "../state/slices/session";
 import { useGetBidsPerUserQuery } from "../state/services/bids.endpoints";
 
-function useUserOffers({ itemsPerPage = 30 }: { itemsPerPage?: number }) {
+function useUserOffers({
+  itemsPerPage = 30,
+  openOffers = undefined,
+}: {
+  itemsPerPage?: number;
+  openOffers?: boolean;
+}) {
   const state = useAppSelector((state) => state);
   const userId = selectId(state);
   const { data, isLoading, isError } = useGetBidsPerUserQuery({
     userId,
     itemsPerPage,
+    openOffers,
   });
 
   return { data, isLoading, isError };

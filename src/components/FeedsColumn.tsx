e8b31264ac91f0b7b1cf8e8d5ce4101d69a63a84
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-01-01 21:17:17 -0500

    Show a message if there are no data to show in the wall

--- src/components/FeedsColumn.tsx
+++ src/components/FeedsColumn.tsx
@@ -6,9 +6,13 @@ function FeedsColumn() {
   const { data, isLoading } = useFeed();
   return (
     <div className="feed-column container-xxl">
-      {isLoading
-        ? [1, 2, 3, 4, 5].map((item) => <FeedOfferPlaceholder key={item} />)
-        : data.map((item) => <FeedOffer key={item.id} item={item} />)}
+      {isLoading ? (
+        [1, 2, 3, 4, 5].map((item) => <FeedOfferPlaceholder key={item} />)
+      ) : data.length > 0 ? (
+        data.map((item) => <FeedOffer key={item.id} item={item} />)
+      ) : (
+        <p className="lead">No hay nada para mostrar</p>
+      )}
     </div>
   );
 }

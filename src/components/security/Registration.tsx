aa44c455e01720c98f0cc06b676909a97eb01264
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-01-04 22:44:00 -0500

    Reduced the lateral padding when in small screens for login/register

--- src/components/security/Registration.tsx
+++ src/components/security/Registration.tsx
@@ -4,7 +4,7 @@ import { pageTitle } from "../../helpers/page-title.helper";
 function Registration() {
   pageTitle("Registro");
   return (
-    <div className="container col-xl-10 col-xxl-8 px-4">
+    <div className="container col-xl-10 col-xxl-8 px-2 px-md-4">
       <div className="row align-items-center py-3">
         <div className="col-md-10 mx-auto col-lg-5">
           <RegistrationForm />

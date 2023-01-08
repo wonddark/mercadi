c4c91b5e0c58b441b9dbd29267d25cb3ee01bf55
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-01-08 12:33:48 -0500

    Implemented the search in the top navbar

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -4,9 +4,10 @@ import LoggedNavItems from "./LoggedNavItems";
 import NotLoggedNavItems from "./NotLoggedNavItems";
 import { Link } from "react-router-dom";
 import { ReactComponent as SiteLogo } from "../../assets/navbar-logo.svg";
+import SearchOffers from "./SearchOffers";
 
 function UserNav() {
-  const isLogged = useSelector(selectIsLogged);
+  const isAuthenticated = useSelector(selectIsLogged);
   return (
     <header className="bg-navbar-dark pb-2 pb-md-0">
       <nav className="navbar navbar-expand navbar-dark bg-navbar-dark">
@@ -16,24 +17,16 @@ function UserNav() {
           </Link>
           <ul className="navbar-nav ms-auto">
             <li className="nav-item d-none d-md-flex me-md-1">
-              <form className="h-100">
-                <input
-                  type="search"
-                  placeholder="Algo"
-                  className="form-control"
-                />
-                <input type="submit" hidden />
-              </form>
+              <SearchOffers />
             </li>
-            {isLogged && <LoggedNavItems />}
-            {!isLogged && <NotLoggedNavItems />}
+            {isAuthenticated && <LoggedNavItems />}
+            {!isAuthenticated && <NotLoggedNavItems />}
           </ul>
         </div>
       </nav>
-      <form className="h-100 d-flex d-md-none mt-1 mx-2">
-        <input type="search" placeholder="Algo" className="form-control" />
-        <input type="submit" hidden />
-      </form>
+      <div className="d-flex d-md-none mt-1 mx-2">
+        <SearchOffers />
+      </div>
     </header>
   );
 }

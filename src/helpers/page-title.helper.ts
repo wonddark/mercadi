9af9dbb6907923ea10b4d6d8337dcfdf364a0d62
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-02-18 14:12:13 -0500

    Removed unnecessary helper

--- src/helpers/page-title.helper.ts
+++
@@ -1,8 +0,0 @@
-const SITE_NAME = "Subastia";
-const SITE_SLOGAN = "";
-
-export const pageTitle = (title?: string) => {
-  document.title = title
-    ? `${title} - ${SITE_NAME}`
-    : `${SITE_NAME} - ${SITE_SLOGAN}`;
-};

6bd5bd4a01dfa3976c85ab2b4e0b71a536e6c27c
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-02-18 13:08:24 -0500

    Transformed the HomeFeed component into a holder for Outlet components

--- src/components/HomeFeed.tsx
+++ src/components/HomeFeed.tsx
@@ -1,9 +1,9 @@
 import FeedQuickAccess from "./FeedQuickAccess";
-import FeedsColumn from "./FeedsColumn";
 import { useAppSelector } from "../hooks/state.hooks";
 import { selectId, selectIsLogged } from "../state/slices/session";
 import { pageTitle } from "../helpers/page-title.helper";
 import MustBeAuthenticated from "./MustBeAuthenticated";
+import { Outlet } from "react-router-dom";
 
 function HomeFeed() {
   const state = useAppSelector((state) => state);
@@ -14,7 +14,7 @@ function HomeFeed() {
     <div className="container-xxl mt-3">
       <div className="row">
         <div className="col col-12 col-lg-8">
-          <FeedsColumn />
+          <Outlet />
         </div>
         <div className="col d-none d-lg-block col-lg-4">
           {isLogged && userId ? <FeedQuickAccess /> : <MustBeAuthenticated />}

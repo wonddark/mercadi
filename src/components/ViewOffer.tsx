dc2d2887418c5fc353359213e1fd94269b192d30
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-02-18 13:20:24 -0500

    Replaced the hardcoded 'go-back' button with the component GoBackBtn

--- src/components/ViewOffer.tsx
+++ src/components/ViewOffer.tsx
@@ -1,21 +1,18 @@
 import { useParams } from "react-router-dom";
 import { useGetOfferByIdQuery } from "../state/services/offers.endpoints";
-import useGoBack from "../hooks/go-back.hooks";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import GoBackBtn from "./common/GoBackBtn";
 
 dayjs.extend(relativeTime);
 
 function ViewOffer() {
   const { offerId } = useParams();
-  const { data, isLoading, isError } = useGetOfferByIdQuery(`${offerId}`);
-  const { goBackTo: goBackToFeed } = useGoBack("/muro");
+  const { data, isLoading } = useGetOfferByIdQuery(`${offerId}`);
   document.title = isLoading ? "Cargando..." : data.name;
   return (
     <div className="container-xxl mt-3">
-      <button className="btn btn-secondary" onClick={goBackToFeed}>
-        <i className="bi bi-arrow-left-circle-fill" />
-      </button>
+      <GoBackBtn to="/muro" />
       {!isLoading ? (
         <div className="mt-4">
           <p className="display-6 text-primary">{data.name}</p>

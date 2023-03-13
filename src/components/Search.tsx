81407fe546cad92822c4b5694c8571c37d6f2dbb
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-13 00:11:19 -0400

    Built the basic layout for search page

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,16 @@
 function Search() {
-  return <></>;
+  return (
+    <div className="container-fluid mt-3">
+      <div className="row">
+        <div className="col col-12 col-md-8 order-1 order-md-0">
+          This will be search result
+        </div>
+        <div className="col col-12 col-md-4 order-0 order-md-1">
+          This will be search form
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;

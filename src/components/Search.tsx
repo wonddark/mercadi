7422188909e6d804ad4c47620fcfb08789d23c41
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-13 18:56:40 -0400

    Fixed the container class used to hold the search page

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import usePageTitle from "../hooks/page-title.hook";
 function Search() {
   usePageTitle({ name: "Buscar" });
   return (
-    <div className="container-fluid mt-3">
+    <div className="container-xxl mt-3">
       <div className="row">
         <div className="col col-12 col-md-8 order-1 order-md-0">
           This will be search result

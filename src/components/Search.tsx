b8479c3013ff1ced59e8143ebb4c7d7ae73cf75d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-13 21:51:52 -0400

    We'll use the search params to custom the offers query for results

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,12 @@
 import usePageTitle from "../hooks/page-title.hook";
+import { useSearchParams } from "react-router-dom";
+import { useLazyGetOffersQuery } from "../state/services/offers.endpoints";
 
 function Search() {
   usePageTitle({ name: "Buscar" });
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+  const [getOffers] = useLazyGetOffersQuery();
   return (
     <div className="container-xxl mt-3">
       <div className="row">

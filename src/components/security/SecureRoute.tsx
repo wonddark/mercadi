6df2123a25580f297dbf10de2573337bab9cde46
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-18 03:19:47 -0400

    Write into the route state the property backTo

--- src/components/security/SecureRoute.tsx
+++ src/components/security/SecureRoute.tsx
@@ -1,13 +1,14 @@
 import { useAppSelector } from "../../hooks/state.hooks";
 import { selectIsLogged } from "../../state/slices/session.slice";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 function SecureRoute({ children }: { children: JSX.Element }) {
   const isLogged = useAppSelector(selectIsLogged);
+  const { pathname } = useLocation();
   if (isLogged) {
     return children;
   } else {
-    return <Navigate to="/acceder" />;
+    return <Navigate to="/acceder" state={{ backTo: pathname }} />;
   }
 }
 

f915863144ceaea47ade60ce365bee1f868eb6c3
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-18 17:57:27 -0400

    Transformed the goBack button into a button link

--- src/components/common/GoBackBtn.tsx
+++ src/components/common/GoBackBtn.tsx
@@ -11,8 +11,8 @@ function useGoBack(to: string) {
 function GoBackBtn({ to }: { to: string }) {
   const { goBackTo: goBackToFeed } = useGoBack(to);
   return (
-    <button className="btn btn-secondary" onClick={goBackToFeed}>
-      <i className="bi bi-arrow-left-circle-fill" />
+    <button className="btn btn-link btn-cancel" onClick={goBackToFeed}>
+      <i className="bi bi-arrow-left" />
     </button>
   );
 }

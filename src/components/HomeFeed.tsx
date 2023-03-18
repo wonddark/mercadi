04416ec2624bedd37a43b2b883058731516bd641
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-18 02:52:50 -0400

    This component does not need to update the page title

--- src/components/HomeFeed.tsx
+++ src/components/HomeFeed.tsx
@@ -1,19 +1,12 @@
 import FeedQuickAccess from "./FeedQuickAccess";
 import { useAppSelector } from "../hooks/state.hooks";
-import {
-  selectId,
-  selectIsLogged,
-  selectName,
-} from "../state/slices/session.slice";
+import { selectId, selectIsLogged } from "../state/slices/session.slice";
 import MustBeAuthenticated from "./MustBeAuthenticated";
 import { Outlet } from "react-router-dom";
-import usePageTitle from "../hooks/page-title.hook";
 
 function HomeFeed() {
   const isLogged = useAppSelector(selectIsLogged);
   const userId = useAppSelector(selectId);
-  const userName = useAppSelector(selectName);
-  usePageTitle({ name: userName });
   return (
     <div className="container-xxl mt-3">
       <div className="row">

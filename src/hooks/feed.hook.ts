e644d5c941412c211d5b0c61d7fb08bd97be1a9a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2023-03-26 01:45:32 -0400

    Fixed the logic used to tell the consumer component that the request is loading

--- src/hooks/feed.hook.ts
+++ src/hooks/feed.hook.ts
@@ -6,25 +6,23 @@ function useFeed() {
   const [response, setResponse] = useState([] as any[]);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(false);
-  const [getOffers, { isLoading }] = useLazyGetOffersQuery();
-
-  const infinityRef = useInfiniteScroll<HTMLDivElement>({
-    next: loadData,
-    rowCount: response.length,
-    hasMore: { down: hasMore },
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [getOffers] = useLazyGetOffersQuery();
 
   async function loadData() {
-    getOffers({ page }).then(({ data }) => {
-      setResponse((prevState) => [...prevState, ...data["hydra:member"]]);
-      const nextPage = data["hydra:view"]["hydra:next"] ? page + 1 : page;
-      if (nextPage !== page) {
-        setPage(nextPage);
-        setHasMore(true);
-      } else {
-        setHasMore(false);
-      }
-    });
+    setIsLoading(true);
+    getOffers({ page })
+      .then(({ data }) => {
+        setResponse((prevState) => [...prevState, ...data["hydra:member"]]);
+        const nextPage = data["hydra:view"]["hydra:next"] ? page + 1 : page;
+        if (nextPage !== page) {
+          setPage(nextPage);
+          setHasMore(true);
+        } else {
+          setHasMore(false);
+        }
+      })
+      .finally(() => setIsLoading(false));
   }
 
   const initialLoad = () => {
@@ -36,6 +34,12 @@ function useFeed() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
+
+  const infinityRef = useInfiniteScroll<HTMLDivElement>({
+    next: loadData,
+    rowCount: response.length,
+    hasMore: { down: hasMore },
+  });
 
   return { data: response, isLoading, infinityRef };
 }
